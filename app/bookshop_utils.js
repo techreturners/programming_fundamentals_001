dc5ee9474f2c718f55d00e4dd5ab729905fa1288
James Heggs <james.heggs@techreturners.com>
2019-11-25 20:30:16 +0000

    Completed task 5 added getFullName function

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,11 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName
 };
